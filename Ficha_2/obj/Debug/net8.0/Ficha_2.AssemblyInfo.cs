6653cdb906a0d0b3d0a8e7f7265e0e3bb0863fb2
Rui Miguel Silva <rui.miguel.silva@coimbra.lip.pt>
2025-03-11 09:09:59 +0000

    New Commit

--- Ficha_2/obj/Debug/net8.0/Ficha_2.AssemblyInfo.cs
+++ Ficha_2/obj/Debug/net8.0/Ficha_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ficha_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02844b4433490984d73ab8c9a079a364e779dcf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ficha_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ficha_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
